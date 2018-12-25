4f628fb9331acbbead8051605312d389421d4138
WangTom <wangtom@WangTomdeMacBook-Pro.local>
2018-12-25 22:15:43 +0800

    update

--- tools/datetime.go
+++ tools/datetime.go
@@ -6,7 +6,7 @@ func GetDate(params ...int64) string {
     res := ""
     format := "2006-01-02"
     if len(params) > 0 {
-        timestamp := string(params[0])
+        timestamp := int64(params[0])
         res = time.Unix(timestamp, 0).Format(format)
     } else {
         res = time.Now().Format(format)
